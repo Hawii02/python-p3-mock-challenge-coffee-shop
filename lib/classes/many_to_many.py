3355286fe10cdfc61d016d20bb2ccc0aa3a37b4f
leon <onditileon21@gmail.com>
2024-03-10 22:16:39 +0300

    still correcting on blockers

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,9 +1,12 @@
 class Coffee:
+    all = []
     def __init__(self, name):
         self.name = name
+        Coffee.all.append(self)
         
     def orders(self):
-        pass
+        return[order for order in Order.all if order.coffee == self]
+        
     
     def customers(self):
         pass
@@ -17,18 +20,26 @@ class Coffee:
 class Customer:
     def __init__(self, name):
         self.name = name
+        self.orders = []
         
     def orders(self):
-        pass
+        return[order for order in self.orders if isinstance(order, Order)]
     
     def coffees(self):
-        pass
+        # coffee_set = set()
+        # for order in self.orders:
+        #     for coffee in order.coffee: 
+        #         if isinstance(coffee, Coffee):
+        #             coffee_set.add(coffee)
+        # return list(coffee_set)
     
     def create_order(self, coffee, price):
         pass
     
 class Order:
+    all = []
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
